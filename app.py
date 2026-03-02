51bb518cf20a37a251276a3d54e9acd9ba14ddc8
rjana3479-coder <rjana3479@gmail.com>
2026-03-02 13:06:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <body style='text-align:center; background-color:#ffe6f0;'>
-        <h1 style='color:purple;'>🌸 Happy Holi Buddy! 🌸</h1>
-        <h2>Play Safe & Enjoy Colors 🎨</h2>
-    </body>
-    """
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
